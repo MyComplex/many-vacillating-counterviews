782e25227c0c430131fa96a718cb1756a22c9911
Jesus Santos <jo_nez@hotmail.com>
2023-09-10 14:46:38 -0700

    added path and handlebars mods

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
+const path = require('path');
 const express = require('express');
+const expressHandlebars = require('express-handlebars');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const handlebars = expressHandlebars.create({});
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-sequelize.sync({ force: true }).then(() => {
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Server listening on port ' + PORT));
 });
