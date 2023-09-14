f5c68822f9ba39947432b964426672611452ec31
Jesus Santos <jo_nez@hotmail.com>
2023-09-14 03:41:09 -0700

    removed signup

--- public/js/logon.js
+++ public/js/logon.js
@@ -19,27 +19,4 @@ const handleLogonForm = async (e) => {
     }
 };
 
-const handleSignupForm = async (e) => {
-    e.preventDefault();
-
-    const username = document.querySelector('#signup-username').value.trim();
-    const password = document.querySelector('#signup-password').value.trim();
-
-    if (username && password) {
-        const response = await fetch('/api/user', {
-            method: 'POST',
-            body: JSON.stringify({ username, password }),
-            headers: { 'Content-Type': 'application/json' },
-        });
-
-        if (response.ok) {
-            document.location.replace('/');
-        } else {
-            alert('A sign up failure has occurred.');
-        }
-    }
-};
-
 document.querySelector('#logon-form').addEventListener('submit', handleLogonForm);
-
-document.querySelector('#signup-form').addEventListener('submit', handleSignupForm);
