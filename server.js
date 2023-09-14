fcc12c4e48f20f8886baba225c0daa87878be27d
Jesus Santos <jo_nez@hotmail.com>
2023-09-13 19:37:45 -0700

    added session storage

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
+const sessionStore = require('connect-session-sequelize')(session.Store);
 const expressHandlebars = require('express-handlebars');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
@@ -12,6 +13,15 @@ const sesh = {
   secret: process.env.SESH_SECRET,
   resave: false,
   saveUninitialized: true,
+  cookie: {
+    maxAge: 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
+  store: new sessionStore({
+    db: sequelize,
+  }),
 };
 
 app.use(session(sesh));
@@ -28,5 +38,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Server listening on port ' + PORT));
+  app.listen(PORT, () => console.log('Server listening on port ' + PORT));
 });
